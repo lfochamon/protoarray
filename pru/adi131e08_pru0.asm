5a3ae329e7bce9d53e5ae2c8aac57d62e2f6f164
Chamon <[luizchamon][gmail]>
2015-06-25 11:34:19 -0300

    Fix assembly to be compatible with TI's compiler.
    Signed-off-by: Chamon <[luizchamon][gmail]>

--- pru/adi131e08_pru0.asm
+++ pru/adi131e08_pru0.asm
@@ -1,71 +1,78 @@
 ; Interrupt
-    PRU_INT_VALID       .set    32
-    PRU0_PRU1_INTERRUPT .set    1       ; PRU_EVTOUT_
-    PRU1_PRU0_INTERRUPT .set    2       ; PRU_EVTOUT_
-    PRU0_ARM_INTERRUPT  .set    3       ; PRU_EVTOUT_0
-    PRU1_ARM_INTERRUPT  .set    4       ; PRU_EVTOUT_1
-    ARM_PRU0_INTERRUPT  .set    5       ; PRU_EVTOUT_
-    ARM_PRU1_INTERRUPT  .set    6       ; PRU_EVTOUT_
+PRU_INT_VALID       .set    32
+PRU0_PRU1_INTERRUPT .set    1       ; PRU_EVTOUT_
+PRU1_PRU0_INTERRUPT .set    2       ; PRU_EVTOUT_
+PRU0_ARM_INTERRUPT  .set    3       ; PRU_EVTOUT_0
+PRU1_ARM_INTERRUPT  .set    4       ; PRU_EVTOUT_1
+ARM_PRU0_INTERRUPT  .set    5       ; PRU_EVTOUT_
+ARM_PRU1_INTERRUPT  .set    6       ; PRU_EVTOUT_
 
 ; Name PRU constants
-    PRU1_RAM            .set    c24
-    PRU0_RAM            .set    c24
-    SHARED_RAM          .set    c28
+PRU1_RAM            .set    c24
+PRU0_RAM            .set    c24
+SHARED_RAM          .set    c28
 
 ; Name PRU register banks
-    XFR_BANK0           .set    10
-    XFR_BANK1           .set    11
-    XFR_BANK2           .set    12
-    XFR_PRU             .set    14
+XFR_BANK0           .set    10
+XFR_BANK1           .set    11
+XFR_BANK2           .set    12
+XFR_PRU             .set    14
 
 
+; Code starts here
+    .text
+    .retain
+    .retainrefs
+    .global         main
 
+; Program start
+main:
 ; Enable the OCP master port
-    LBCO    r0, C4, 4, 4    ; Load SYSCFG into r0 using c4 constant
+    LBCO    &r0, C4, 4, 4   ; Load SYSCFG into r0 using c4 constant
     CLR     r0, r0, 4       ; Clear STANDBY_INIT (bit 4)
-    SBCO    r0, C4, 4, 4    ; Store modified SYSCFG back
+    SBCO    &r0, C4, 4, 4   ; Store modified SYSCFG back
 
 ; Set c24 = 0 and c25 = 0x2000 (data RAM in PRU1 and PRU0)
-    MOV     r0, 0x24020     ; Load CTBIR0 address in r0
-    MOV     r1, 0           ; c24 = 0x00000n00, c25 = 0x00002n00
-    SBBO    r1, r0, 0, 4    ; Store r1 into CTBIR0 (pointer r0)
+    LDI32   r0, 0x24020     ; Load CTBIR0 address in r0
+    LDI     r1, 0           ; c24 = 0x00000n00, c25 = 0x00002n00
+    SBBO    &r1, r0, 0, 4   ; Store r1 into CTBIR0 (pointer r0)
 
 ; Set C28 = 0x00010000 (shared PRU RAM)
-    MOV     r0, 0x24028     ; Load CTPPR0 address in r0
-    MOV     r1, 0x0100      ; c28 = 0x00nnnn00
-    SBBO    r1, r0, 0, 4    ; Store r1 into CTBIR0 (pointer r0)
+    LDI32   r0, 0x24028     ; Load CTPPR0 address in r0
+    LDI     r1, 0x0100      ; c28 = 0x00nnnn00
+    SBBO    &r1, r0, 0, 4   ; Store r1 into CTBIR0 (pointer r0)
 
 ; Setup auxiliary registers
-    MOV     r0, 18          ; Load PRU1 interrupt number in r0 (PRU SRM p. 222)
-    LBCO    r28, c24, 0, 4  ; Load DDR RAM address in r28
+    LDI     r0, 18          ; Load PRU1 interrupt number in r0 (PRU SRM p. 222)
+    LBCO    &r28, c24, 0, 4 ; Load DDR RAM address in r28
 
 
 ; Start main loop
-MAINLOOP:
-    MOV     r29, 0                  ; Initialize byte counter in r29
+mainloop:
+    LDI     r29, 0                  ; Initialize byte counter in r29
 
-    WBS     R30, 30                 ; Wait for PRU1 interrupt signal
-    SBCO    r0, C0, 0x24, 4         ; Clear PRU1 interrupt
+    WBS     r30, 30                 ; Wait for PRU1 interrupt signal
+    SBCO    &r0, C0, 0x24, 4        ; Clear PRU1 interrupt
 
     XIN     XFR_BANK0, r1, 100      ; Retrieve data from scratch pad
-    SBCO    r1, r28, r29, 100       ; Write data to DDR RAM
+    SBCO    &r1, r28, r29, 100      ; Write data to DDR RAM
     ADD     r29, r29, 100
 
 ; Send interrupt to host
-    MOV     R31.b0, PRU_INT_VALID | PRU0_ARM_INTERRUPT
+    LDI     r31.b0, PRU_INT_VALID + PRU0_ARM_INTERRUPT
 
 
-    WBS     R30, 30                 ; Wait for PRU1 interrupt signal
-    SBCO    r0, C0, 0x24, 4         ; Clear PRU1 interrupt
+    WBS     r30, 30                 ; Wait for PRU1 interrupt signal
+    SBCO    &r0, C0, 0x24, 4        ; Clear PRU1 interrupt
 
     XIN     XFR_BANK0, r1, 100      ; Retrieve data from scratch pad
-    SBCO    r1, r28, r29, 100       ; Write data to DDR RAM
+    SBCO    &r1, r28, r29, 100      ; Write data to DDR RAM
     ADD     r29, r29, 100
 
 ; Send interrupt to host
-    MOV     R31.b0, PRU_INT_VALID | PRU0_ARM_INTERRUPT
+    LDI     r31.b0, PRU_INT_VALID + PRU0_ARM_INTERRUPT
 
-    QBA     MAINLOOP                ; [TODO]: make loop conditional
+    QBA     mainloop                ; [TODO]: make loop conditional
 
 
     ; Stop PRU
--- pru/adi131e08_pru1.asm
+++ pru/adi131e08_pru1.asm
@@ -1,64 +1,74 @@
 ; Delay
-    INS_PER_US          .set    200
-    INS_PER_DELAY_LOOP  .set    2
-    DELAY_US            .set    119 * (INS_PER_US / INS_PER_DELAY_LOOP)
+INS_PER_US          .set    200
+INS_PER_DELAY_LOOP  .set    2
+DELAY_US            .set    119 * (INS_PER_US / INS_PER_DELAY_LOOP)
 
 ; Interrupt
-    PRU_INT_VALID       .set    32
-    PRU0_PRU1_INTERRUPT .set    1       ; PRU_EVTOUT_
-    PRU1_PRU0_INTERRUPT .set    2       ; PRU_EVTOUT_
-    PRU0_ARM_INTERRUPT  .set    3       ; PRU_EVTOUT_0
-    PRU1_ARM_INTERRUPT  .set    4       ; PRU_EVTOUT_1
-    ARM_PRU0_INTERRUPT  .set    5       ; PRU_EVTOUT_
-    ARM_PRU1_INTERRUPT  .set    6       ; PRU_EVTOUT_
+PRU_INT_VALID       .set    32
+PRU0_PRU1_INTERRUPT .set    1       ; PRU_EVTOUT_
+PRU1_PRU0_INTERRUPT .set    2       ; PRU_EVTOUT_
+PRU0_ARM_INTERRUPT  .set    3       ; PRU_EVTOUT_0
+PRU1_ARM_INTERRUPT  .set    4       ; PRU_EVTOUT_1
+ARM_PRU0_INTERRUPT  .set    5       ; PRU_EVTOUT_
+ARM_PRU1_INTERRUPT  .set    6       ; PRU_EVTOUT_
 
 ; Name PRU register banks
-    XFR_BANK0           .set    10
-    XFR_BANK1           .set    11
-    XFR_BANK2           .set    12
-    XFR_PRU             .set    14
+XFR_BANK0           .set    10
+XFR_BANK1           .set    11
+XFR_BANK2           .set    12
+XFR_PRU             .set    14
+
+
+; Code starts here
+    .text
+    .retain
+    .retainrefs
+    .global         main
+
+; Program start
+main:
 
 ; Start of main loop
-MAINLOOP:
+mainloop:
     ; Wait and retrieve data
-    MOV r1,  1
-    MOV r2,  2
-    MOV r3,  3
-    MOV r4,  4
-    MOV r5,  5
-    MOV r6,  6
-    MOV r7,  7
-    MOV r8,  8
-    MOV r9,  9
-    MOV r10, 10
-    MOV r11, 11
-    MOV r12, 12
-    MOV r13, 13
-    MOV r14, 14
-    MOV r15, 15
-    MOV r16, 16
-    MOV r17, 17
-    MOV r18, 18
-    MOV r19, 19
-    MOV r20, 20
-    MOV r21, 21
-    MOV r22, 22
-    MOV r23, 23
-    MOV r24, 24
-    MOV r25, 25
+    LDI r1,  1
+    LDI r2,  2
+    LDI r3,  3
+    LDI r4,  4
+    LDI r5,  5
+    LDI r6,  6
+    LDI r7,  7
+    LDI r8,  8
+    LDI r9,  9
+    LDI r10, 10
+    LDI r11, 11
+    LDI r12, 12
+    LDI r13, 13
+    LDI r14, 14
+    LDI r15, 15
+    LDI r16, 16
+    LDI r17, 17
+    LDI r18, 18
+    LDI r19, 19
+    LDI r20, 20
+    LDI r21, 21
+    LDI r22, 22
+    LDI r23, 23
+    LDI r24, 24
+    LDI r25, 25
 
 
     ; Wait 119 us
-    MOV     r0, DELAY_US
+    LDI32   r0, DELAY_US
     DELAY:
         SUB     r2, r0, 1
         QBNE    DELAY, r0, 0
 
 
     XOUT    XFR_BANK0, r1, 100                          ; Send data to scratch pad
-    MOV     R31.b0, PRU_INT_VALID | PRU1_PRU0_INTERRUPT ; Send interrupt to PRU0
+    LDI     r31.b0, PRU_INT_VALID + PRU1_PRU0_INTERRUPT ; Send interrupt to PRU0
 
-    QBA MAINLOOP            ; [TODO]: make loop conditional
+    QBA mainloop            ; [TODO]: make loop conditional
 
     ; Stop PRU
     HALT
