d5e7b7699b79918fbfcae4df8e640030bef020d1
Chamon <luizchamon@gmail.com>
2015-06-26 22:46:14 -0300

    Port PRU1 code to TI's compiler.
    Signed-off-by: Chamon <luizchamon@gmail.com>

--- pru/adi131e08_pru1.asm
+++ pru/adi131e08_pru1.asm
@@ -1,64 +1,75 @@
-.origin 0                        // start of program in PRU memory
+; Delay
+INS_PER_US          .set    200
+INS_PER_DELAY_LOOP  .set    2
+DELAY_US            .set    119 * (INS_PER_US / INS_PER_DELAY_LOOP)
 
-/* Pin definitions */
-#define SPI_SCLK    r30.t5 /* PRU1_5 GPIO2_11 P8_42 */
-#define SPI_MOSI    r30.t0 /* PRU1_0 GPIO2_6  P8_45 */ /* DIN */
-#define SPI_MISO    r31.t3 /* PRU1_3 GPIO2_9  P8_44 */ /* DOUT */
-#define SPI_CS      r30.t7 /* PRU1_7 GPIO2_13 P8_40 */
-#define ADI_START   r30.t4 /* PRU1_4 GPIO2_10 P8_41 */
-#define ADI_RESET   r30.t2 /* PRU1_2 GPIO2_8  P8_43 */
-#define ADI_DRDY    r31.t1 /* PRU1_1 GPIO2_7  P8_46 */
+; Pin definitions
+SPI_SCLK            .set    5  ; PRU1_5 GPIO2_11 P8_42
+SPI_MOSI            .set    0  ; PRU1_0 GPIO2_6  P8_45 (DIN)
+SPI_MISO            .set    3  ; PRU1_3 GPIO2_9  P8_44 (DOUT)
+SPI_CS              .set    7  ; PRU1_7 GPIO2_13 P8_40
+ADI_START           .set    4  ; PRU1_4 GPIO2_10 P8_41
+ADI_RESET           .set    2  ; PRU1_2 GPIO2_8  P8_43
+ADI_DRDY            .set    1  ; PRU1_1 GPIO2_7  P8_46
 
-/* SPI_SCLK_DELAY = floor( (t_SCLK / 5 ns) / 4 ); SPI_SCLK_DELAY >= 3 */
-#define SPI_SCLK_DELAY 9
+; SPI_SCLK_DELAY = floor( (t_SCLK / 5 ns) / 4 ); SPI_SCLK_DELAY >= 3
+SPI_SCLK_DELAY      .set    9
 
-/* Interrupt */
-#define PRU_INT_VALID 32
-#define PRU0_PRU1_INTERRUPT 1   // PRU_EVTOUT_
-#define PRU1_PRU0_INTERRUPT 2   // PRU_EVTOUT_
-#define PRU0_ARM_INTERRUPT 3    // PRU_EVTOUT_0
-#define PRU1_ARM_INTERRUPT 4    // PRU_EVTOUT_1
-#define ARM_PRU0_INTERRUPT 5    // PRU_EVTOUT_
-#define ARM_PRU1_INTERRUPT 6    // PRU_EVTOUT_
+; Interrupt
+PRU_INT_VALID       .set    32
+PRU0_PRU1_INTERRUPT .set    1       ; PRU_EVTOUT_
+PRU1_PRU0_INTERRUPT .set    2       ; PRU_EVTOUT_
+PRU0_ARM_INTERRUPT  .set    3       ; PRU_EVTOUT_0
+PRU1_ARM_INTERRUPT  .set    4       ; PRU_EVTOUT_1
+ARM_PRU0_INTERRUPT  .set    5       ; PRU_EVTOUT_
+ARM_PRU1_INTERRUPT  .set    6       ; PRU_EVTOUT_
 
-/* Name PRU register banks */
-#define XFR_BANK0 10
-#define XFR_BANK1 11
-#define XFR_BANK2 12
-#define XFR_PRU 14
-
-#include "adi131e08.h"
+; Name PRU register banks
+XFR_BANK0           .set    10
+XFR_BANK1           .set    11
+XFR_BANK2           .set    12
+XFR_PRU             .set    14
 
 
-/* Start up sequence for the ADI */
-ADI_STARTUP
+; Code starts here
+    .text
+    .retain
+    .retainrefs
+    .global         main
 
-/* Activate internal clock */
-ADI_WRITE_REG   CONFIG13, CONFIG3_MASK | PDB_REFBUF
-ADI_WAIT        2000       // Wait 20 us for the internal clock to start up
+; Include ADI131 driver
+    .include "adi131e08.inc"
 
-/* Configure ADI */
-ADI_WRITE_REG   CONFIG1, CONFIG1_MASK | DR_32K    // 16 bits @ 32 kHz, daisy-chain, disable clock output
-ADI_WRITE_REG   CONFIG2, CONFIG2_MASK             // external test signal, gain 1x, fclk / 2^21 (all defaults)
-ADI_WRITE_REG   CH1SET,  CH_GAIN_1 | CH_SHORTED   // Channel 1 (PGA gain 1x, channel shorted)
-ADI_WRITE_REG   CH2SET,  CH_GAIN_1 | CH_SHORTED   // Channel 2 (PGA gain 1x, channel shorted)
-ADI_WRITE_REG   CH3SET,  CH_GAIN_1 | CH_SHORTED   // Channel 3 (PGA gain 1x, channel shorted)
-ADI_WRITE_REG   CH4SET,  CH_GAIN_1 | CH_SHORTED   // Channel 4 (PGA gain 1x, channel shorted)
-ADI_WRITE_REG   CH5SET,  CH_GAIN_1 | CH_SHORTED   // Channel 5 (PGA gain 1x, channel shorted)
-ADI_WRITE_REG   CH6SET,  CH_GAIN_1 | CH_SHORTED   // Channel 6 (PGA gain 1x, channel shorted)
-ADI_WRITE_REG   CH7SET,  CH_GAIN_1 | CH_SHORTED   // Channel 7 (PGA gain 1x, channel shorted)
-ADI_WRITE_REG   CH8SET,  CH_GAIN_1 | CH_SHORTED   // Channel 8 (PGA gain 1x, channel shorted)
+main:
+; Start up sequence for the ADI
+    ADI_STARTUP
 
-/* Put ADI back in continuous data conversion mode */
-MOV     r1.b0, RDATAC
-SPI_TX  r1.b0
+; Activate internal clock
+    ADI_WRITE_REG   CONFIG3, CONFIG3_MASK + PDB_REFBUF
+    ADI_WAIT        2000       ; Wait 20 us for the internal clock to start up
 
-/* START = 1 */
-SET     ADI_START
+; Configure ADI
+    ADI_WRITE_REG   CONFIG1, CONFIG1_MASK + DR_32K      ; 16 bits @ 32 kHz, daisy-chain, disable clock output
+    ADI_WRITE_REG   CONFIG2, CONFIG2_MASK               ; external test signal, gain 1x, fclk / 2^21 (all defaults)
+    ADI_WRITE_REG   CH1SET,  CH_GAIN_1 + CH_SHORTED     ; Channel 1 (PGA gain 1x, channel shorted)
+    ADI_WRITE_REG   CH2SET,  CH_GAIN_1 + CH_SHORTED     ; Channel 2 (PGA gain 1x, channel shorted)
+    ADI_WRITE_REG   CH3SET,  CH_GAIN_1 + CH_SHORTED     ; Channel 3 (PGA gain 1x, channel shorted)
+    ADI_WRITE_REG   CH4SET,  CH_GAIN_1 + CH_SHORTED     ; Channel 4 (PGA gain 1x, channel shorted)
+    ADI_WRITE_REG   CH5SET,  CH_GAIN_1 + CH_SHORTED     ; Channel 5 (PGA gain 1x, channel shorted)
+    ADI_WRITE_REG   CH6SET,  CH_GAIN_1 + CH_SHORTED     ; Channel 6 (PGA gain 1x, channel shorted)
+    ADI_WRITE_REG   CH7SET,  CH_GAIN_1 + CH_SHORTED     ; Channel 7 (PGA gain 1x, channel shorted)
+    ADI_WRITE_REG   CH8SET,  CH_GAIN_1 + CH_SHORTED     ; Channel 8 (PGA gain 1x, channel shorted)
 
-/* Start of main loop */
-MAINLOOP:
-    /* Wait and retrieve data */
+; Put ADI back in continuous data conversion mode
+    LDI     r1.b0, RDATAC
+    SPI_TX  r1.b0
+
+; START = 1
+    SET     r30, r30, ADI_START
+
+; Start of main loop
+mainloop:
+    ; Wait and retrieve data
     ADI_GET_DATA16  r1, r2, r3, r4, r5
     ADI_GET_DATA16  r1, r6, r7, r8, r9
     ADI_GET_DATA16  r1, r10, r11, r12, r13
@@ -66,14 +77,10 @@ MAINLOOP:
     ADI_GET_DATA16  r1, r18, r19, r20, r21
     ADI_GET_DATA16  r1, r22, r23, r24, r25
 
-    XOUT    XFR_BANK0, R1, 100                          // Send data to scratch pad
-    MOV     R31.b0, PRU_INT_VALID | PRU1_PRU0_INTERRUPT // Send interrupt to PRU0
+    XOUT    XFR_BANK0, &r1, 100                         ; Send data to scratch pad
+    LDI     r31.b0, PRU_INT_VALID + PRU1_PRU0_INTERRUPT ; Send interrupt to PRU0
 
-    QBA MAINLOOP        // [TODO]: make loop conditional
+    JMP mainloop        ; [TODO]: make loop conditional
 
-
-/* Send interrupt to host*/
-MOV     R31.b0, PRU_INT_VALID | PRU1_ARM_INTERRUPT
-
-/* Stop PRU */
-HALT
+; Stop PRU
+    HALT
